e3b0f05019ab24900d94ecfb9cbf707586776029
Kyle Swanson <swansonk.14@gmail.com>
2023-11-03 22:27:48 -0700

    Adding utils with data preprocessing

---
+++ admet_ai/utils.py
@@ -0,0 +1,31 @@
+"""Utility functions for ADMET-AI."""
+from pathlib import Path
+
+import pandas as pd
+
+
+def load_and_preprocess_data(data_path: Path, smiles_column: str = "smiles") -> pd.DataFrame:
+    """Preprocess a dataset of molecules by removing missing SMILES and setting the SMILES as the index.
+
+    :param data_path: Path to a CSV file containing a dataset of molecules.
+    :param smiles_column: Name of the column containing SMILES strings.
+    :return: A DataFrame containing the preprocessed data with SMILES strings as the index.
+    """
+    # Load data
+    data = pd.read_csv(data_path)
+
+    # Remove missing SMILES
+    original_num_molecules = len(data)
+    data.dropna(subset=[smiles_column], inplace=True, ignore_index=True)
+
+    # Warn if molecules were removed
+    if len(data) < original_num_molecules:
+        print(
+            f"Warning: {original_num_molecules - len(data):,} molecules were removed "
+            f"from the dataset because they were missing SMILES."
+        )
+
+    # Set SMILES as index
+    data.set_index(smiles_column, inplace=True)
+
+    return data
