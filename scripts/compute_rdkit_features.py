3d9850ce6123bf1fd2918b11dcf781b274b37939
davide-grheco <davide.crucitti@sergas.es>
2024-10-31 14:34:03 +0100

    Save fingerprints in an npz supported by Chemprop v2

--- scripts/compute_rdkit_features.py
+++ scripts/compute_rdkit_features.py
@@ -1,8 +1,43 @@
 """Compute RDKit features for the Therapeutics Data Commons (TDC) ADMET datasets"""
+
+import shutil
 import subprocess
 from pathlib import Path
+from typing import Literal
 
+from chemfunc import compute_fingerprints
+
+import numpy as np
+import pandas as pd
 from tqdm import tqdm
+
+
+# From ChemFunc
+def save_fingerprints(
+    data_path: Path,
+    save_path: Path,
+    smiles_column: str,
+    fingerprint_type: Literal["morgan", "rdkit"] = "rdkit",
+) -> None:
+    """Saves fingerprints for molecules in a dataset.
+
+    :param data_path: Path to a CSV file containing molecules.
+    :param save_path: Path to a NPZ file where the fingerprints are saved
+    :param fingerprint_type: The type of fingerprint to compute.
+    :param smiles_column: Name of column containing SMILES strings.
+    """
+    # Load data
+    data = pd.read_csv(data_path)
+
+    # Get SMILES
+    smiles = data[smiles_column].tolist()
+
+    # Compute fingerprints
+    fingerprints = compute_fingerprints(mols=smiles, fingerprint_type=fingerprint_type)
+
+    # Save fingerprints
+    save_path.parent.mkdir(parents=True, exist_ok=True)
+    np.savez(save_path, fingerprints)
 
 
 def compute_rdkit_features(data_dir: Path, smiles_column: str) -> None:
@@ -16,19 +51,11 @@ def compute_rdkit_features(data_dir: Path, smiles_column: str) -> None:
 
     # Compute features for each dataset using chemfunc
     for data_path in tqdm(data_paths):
-        subprocess.run(
-            [
-                "chemfunc",
-                "save_fingerprints",
-                "--data_path",
-                str(data_path),
-                "--save_path",
-                str(data_path.with_suffix(".npz")),
-                "--smiles_column",
-                smiles_column,
-                "--fingerprint_type",
-                "rdkit",
-            ]
+        save_fingerprints(
+            data_path=data_path,
+            save_path=data_path.with_suffix(".npz"),
+            smiles_column=smiles_column,
+            fingerprint_type="rdkit",
         )
 
 
